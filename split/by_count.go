33000704b1af53dd636fb46c4250cd3fdc52f0ac
iliya <iliyahashemkhani@gmail.com>
2023-11-03 19:27:49 +0330

    Update by_count.go

--- split/by_count.go
+++ split/by_count.go
@@ -1,21 +1,42 @@
 package split
 
-func ByCount(list *[]string, count int) *[][]string {
-
-	data := *list
+func ByCount(list []string, count int) *[][]string {
 
 	result := [][]string{}
 
-	min_index := 0
-	max_index := len(data)/count + 1
+	switch len(list) {
 
-	for i := 0; i < count; i++ {
+	case 0:
+		return &[][]string{}
 
-		result = append(result, []string{})
-		result[i] = data[min_index:max_index]
-		min_index, max_index = max_index, max_index+len(data)/count
+	case 1:
+		return &[][]string{{list[0]}}
 
+	case count:
+		for _, v := range list {
+
+			result = append(result, []string{v})
+		}
+
+		return &result
+
+	default:
+		min_index := 0
+		max_index := len(list) / count
+
+		if len(list)%count != 0 {
+
+			max_index++
+		}
+
+		for i := 0; i < count; i++ {
+
+			result = append(result, []string{})
+			result[i] = list[min_index:max_index]
+			min_index, max_index = max_index, max_index+len(list)/count
+
+		}
+
+		return &result
 	}
-
-	return &result
 }
