9583bfb9fbd53222828d49c322ae901c852b876a
iliya <iliyahashemkhani@gmail.com>
2023-12-29 13:20:36 +0330

    Update log.go

--- log/log.go
+++ log/log.go
@@ -8,35 +8,49 @@ import (
 	"github.com/fatih/color"
 )
 
+var SILENT bool
+
 func Trace(s ...string) {
 
-	logger := log.New(os.Stderr, "", 0)
-	logger.Println(strings.Join(s, " "))
+	if !SILENT {
+
+		logger := log.New(os.Stderr, "", 0)
+		logger.Println(strings.Join(s, " "))
+	}
 }
 
 func Info(s ...string) {
 
-	logger := log.New(os.Stderr, "", 0)
-	coloredPrefix := color.New(color.FgBlue).SprintFunc()("[INF] ")
+	if !SILENT {
 
-	logger.SetPrefix(coloredPrefix)
-	logger.Println(strings.Join(s, " "))
+		logger := log.New(os.Stderr, "", 0)
+		coloredPrefix := color.New(color.FgBlue).SprintFunc()("[INF] ")
+
+		logger.SetPrefix(coloredPrefix)
+		logger.Println(strings.Join(s, " "))
+	}
 }
 
 func Warn(s ...string) {
 
-	logger := log.New(os.Stderr, "", 0)
-	coloredPrefix := color.New(color.FgYellow).SprintFunc()("[WRN] ")
+	if SILENT {
 
-	logger.SetPrefix(coloredPrefix)
-	logger.Println(strings.Join(s, " "))
+		logger := log.New(os.Stderr, "", 0)
+		coloredPrefix := color.New(color.FgYellow).SprintFunc()("[WRN] ")
+
+		logger.SetPrefix(coloredPrefix)
+		logger.Println(strings.Join(s, " "))
+	}
 }
 
 func Error(s ...string) {
 
-	logger := log.New(os.Stderr, "", 0)
-	coloredPrefix := color.New(color.FgRed).SprintFunc()("[ERR] ")
+	if SILENT {
 
-	logger.SetPrefix(coloredPrefix)
-	logger.Println(strings.Join(s, " "))
+		logger := log.New(os.Stderr, "", 0)
+		coloredPrefix := color.New(color.FgRed).SprintFunc()("[ERR] ")
+
+		logger.SetPrefix(coloredPrefix)
+		logger.Println(strings.Join(s, " "))
+	}
 }
